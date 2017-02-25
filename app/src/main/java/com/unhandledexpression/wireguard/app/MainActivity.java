82eaba639f65c64370b60cb13b0f4a8321efa5a4
Geoffroy Couprie <geo.couprie@gmail.com>
2017-02-25 13:07:49 +0100

    project structure

---
+++ app/src/main/java/com/unhandledexpression/wireguard/app/MainActivity.java
@@ -0,0 +1,15 @@
+package com.unhandledexpression.wireguard.app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.unhandledexpression.wireguard.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/com/unhandledexpression/wireguard/protocol/State.java
@@ -0,0 +1,8 @@
+package com.unhandledexpression.wireguard.protocol;
+
+/**
+ * Created by geal on 25/02/2017.
+ */
+
+public class State {
+}
