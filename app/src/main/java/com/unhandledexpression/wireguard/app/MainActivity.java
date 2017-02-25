decb0e48f0d9fe96f6765062d689a49a38a5d9f6
Geoffroy Couprie <geo.couprie@gmail.com>
2017-02-25 18:40:28 +0100

    test in the main activity

--- app/src/main/java/com/unhandledexpression/wireguard/app/MainActivity.java
+++ app/src/main/java/com/unhandledexpression/wireguard/app/MainActivity.java
@@ -1,9 +1,14 @@
 package com.unhandledexpression.wireguard.app;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
 
 import com.unhandledexpression.wireguard.R;
+import com.unhandledexpression.wireguard.protocol.Hardcoded;
+import com.unhandledexpression.wireguard.protocol.State;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +16,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final TextView txt = (TextView) findViewById(R.id.txt);
+
+        (new AsyncTask<Void, Void, Void>() {
+            @Override
+            protected Void doInBackground(Void[] objects) {
+                final State state = new State(Hardcoded.myPrivateKey, Hardcoded.serverPublicKey);
+
+                Log.d("wg", "state: "+state.toString());
+
+                state.initiate();
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        txt.setText(state.toString());
+                    }
+                });
+                return null;
+            }
+        }).execute();
+
     }
 }
