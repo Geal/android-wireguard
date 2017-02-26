fece081eb73193d17a4434ad1ca41fa9a88f6752
Geoffroy Couprie <geo.couprie@gmail.com>
2017-02-26 18:19:28 +0100

    new method for hexdumps

--- app/src/main/java/com/unhandledexpression/wireguard/Utils.java
+++ app/src/main/java/com/unhandledexpression/wireguard/Utils.java
@@ -1,5 +1,7 @@
 package com.unhandledexpression.wireguard;
 
+import java.io.UnsupportedEncodingException;
+
 /**
  * Created by geal on 25/02/2017.
  */
@@ -14,5 +16,37 @@ public class Utils {
             hexChars[j * 2 + 1] = hexArray[v & 0x0F];
         }
         return new String(hexChars);
+    }
+
+    public static String formatHexDump(byte[] array, int offset, int length) {
+        final int width = 16;
+
+        StringBuilder builder = new StringBuilder();
+
+        for (int rowOffset = offset; rowOffset < offset + length; rowOffset += width) {
+            builder.append(String.format("%06d:  ", rowOffset));
+
+            for (int index = 0; index < width; index++) {
+                if (rowOffset + index < array.length) {
+                    builder.append(String.format("%02x ", array[rowOffset + index]));
+                } else {
+                    builder.append("   ");
+                }
+            }
+
+            if (rowOffset < array.length) {
+                int asciiWidth = Math.min(width, array.length - rowOffset);
+                builder.append("  |  ");
+                try {
+                    builder.append(new String(array, rowOffset, asciiWidth, "UTF-8").replaceAll("\r\n", " ").replaceAll("\n", " "));
+                } catch (UnsupportedEncodingException ignored) {
+                    //If UTF-8 isn't available as an encoding then what can we do?!
+                }
+            }
+
+            builder.append(String.format("%n"));
+        }
+
+        return builder.toString();
     }
 }
